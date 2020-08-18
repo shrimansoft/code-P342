a1d0645ca045f8713850e51bbb8f74b45b6bccb8
shrimansoft <shrimansoft@hotmail.com>
2020-08-18 23:59:08 +0530

    Revert "Revert "don all""
    This reverts commit b9e43759683baa9846f9ad9c0f76113f8e6eaf08.

--- Assignment_2/Q1a.cpp
+++ Assignment_2/Q1a.cpp
@@ -5,21 +5,28 @@ using namespace std;
 
 double averageDistance(double* );
 
-
+//-------------------------* main function to run tests *-------------------------
 int main()
 {
-    double points[6] = {1,2,3,4,5,6};
-    cout<<"Average distance of points [ 1 , 2, 3 , 4, 5 ,6 ] is :" << averageDistance(points)<<endl;
+    double points1[6] = {1,2,3,4,5,6};    // double[] varible to store the points with unit distance 
+    cout<<"Average distance of points [ 1 , 2, 3 , 4, 5 ,6 ] is :" << averageDistance(points1)<<endl;
+
+    double points2[6] = {3,4,5,6,7,8};   // double[] variable store points with unit distance (displaced by 2 unit)
+    cout<<"Average distance of points [ 3 , 4, 5 ,6 , 7, 8 ] is :" << averageDistance(points2)<<endl;
 }
 
-
+//-------------------------* averageDistance funtion to find the averageDistance between 6 points *-------------------------
 double  averageDistance(double a[])
 {
-    double sum = 0;
-    for(int i=0;i<6;i++)
-    for(int j=0;j<6;j++)
+    double sum=0;           //  Variable to store the sum during loop
+    for(int i=0;i<6;i++)    //  fixing one of the points out of six
+    for(int j=0;j<6;j++)    //  Running over the rest of the points including fixed point
     {
-        sum+= abs(a[j]-a[i]);
+        sum+= abs(a[j]-a[i]);    // Adding the absolute distance between the points.
     }
-    return (sum/36);
+    return (sum/36);         // returing the average distance of points.
 }
+
+//------------------------* Output of the code *--------------------------------------------
+//Average distance of points [ 1 , 2, 3 , 4, 5 ,6 ] is :1.94444
+//Average distance of points [ 3 , 4, 5 ,6 , 7, 8 ] is :1.94444
